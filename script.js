a56ca35db4a0383c1ffdea49f358840c92ab85f2
h-cheema <harjinder.sc96@gmail.com>
2021-04-13 12:56:21 -0400

    App now has default input values. Visualizer now shows/hides elements based on the validitiy of input values. App is almost fully functional at this point.

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 let globals = {
     form: { // Form values.
-        dateStart: null,
-        dateToday: null,
-        numberOfYears: null,
+        dateStart: "1990-01-01",
+        numberOfYears: 80,
+        dateToday: getCurrentDate(),
     },
     visualizer: {
         days: null,
         weeks: null,
-    }
+    },
 };
 
 $(document).ready(function() {
+
+    initFormValues();
 
     let inputForm = document.getElementById("inputForm");
     inputForm.addEventListener('input', (e) => {
@@ -30,31 +32,24 @@ $(document).ready(function() {
 
             let date1 = new Date (globals.form.dateStart);
             let date2 = new Date (globals.form.dateToday);
-            calculateDays(date1, date2);
-
+            
+            let valuesAreValid = false;
+            if (date1.getTime() < date2.getTime()) {
+                calculateDays(date1, date2);
+                valuesAreValid = true;
+            }
+            
+            renderVisualizer(valuesAreValid);
         }
-
     });
-
-    // renderVisualizer();
-
 });
 
-function renderVisualizer() {
+function initFormValues() {
+    document.getElementById("dateStart").value = globals.form.dateStart;
+    document.getElementById("numberOfYears").value = globals.form.numberOfYears;
+    document.getElementById("dateToday").value = globals.form.dateToday;
 
-    let weeks = 100;
-    let cell = "O";
-    let vCells = document.getElementById('v-cells');
-    let vHeader = document.getElementById('v-header');
 
-    for (let i = 0; i < weeks; i++) {
-        vCells.innerText = vCells.innerText + cell;
-    }
-
-    vHeader.innerText = weeks + " weeks";
-    
-    console.log("visualizer rendered.");
-    
 }
 
 function isFormValid() {
@@ -76,20 +71,48 @@ function isFormValid() {
         console.log("Form is not valid.");
         return false;
     }
-    
 }
 
+// Renders when form values are valid.
+function renderVisualizer(valuesAreValid) {
+
+    let weeks = globals.visualizer.weeks;
+    let cellText = "O";
+    let cellInnerText = "";
+
+    let vHeader = document.getElementById('v-header');
+    let vCells = document.getElementById('v-cells');
+    vCells.innerText = cellInnerText;
+
+    if (valuesAreValid) {
+        // Render the visualizer.
+        for (let i = 0; i < weeks; i++) {
+            cellInnerText = cellInnerText + cellText;
+        }
+        vCells.innerText = cellInnerText;
+        vHeader.innerText = weeks + " weeks (" + Math.round(weeks/52 * 100)/100 + " years)"; // round 2 dec
+    } else {
+        // Reset the visualizer and display message.
+        vHeader.innerText = "Please pick a valid date range.";
+    }
+
+}
 
 /** Calculate weeks */
 function calculateDays(date1, date2) {
+    
     var time = date2.getTime() - date1.getTime();
     var days = time / (1000 * 3600 * 24);
 
-    globals.visualizer.days = days;
-    globals.visualizer.weeks = days/7;
+    globals.visualizer.days = Math.round(days); 
+    globals.visualizer.weeks = Math.round(days/7);
 
     console.log("globals.visualizer.days: " + globals.visualizer.days);
     console.log("globals.visualizer.weeks: " + globals.visualizer.weeks);
+}
+
+function getCurrentDate() {
+    return new Date().toISOString().slice(0, 10);
 }
 
 function getObjectLength(obj) {
