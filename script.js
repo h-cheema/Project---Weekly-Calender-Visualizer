4e73d530478513bd3a1fa388fea3db0ccdd2a1e7
h-cheema <harjinder.sc96@gmail.com>
2021-04-13 01:56:16 -0400

    Started working on javascript for the back end of the visualizer.

--- script.js
+++ script.js
@@ -1,4 +1,39 @@
-$(document).ready(function() {
+$( document ).ready(function() {
 
-    
+
+    // Get form input
+    let dateStart = document.getElementById("dateStart");
+    let dateStartVal;
+
+    let numberOfYears = document.getElementById("numberOfYears");
+    let numberOfYearsVal;
+
+    let dateToday = document.getElementById("dateToday");
+    let dateTodayVal;
+
+
+    dateStart.addEventListener('input', () => {
+        console.log(dateStart.value);
+    });
+    numberOfYears.addEventListener('input', () => {
+        console.log(dateStart.value);
+    });
+    dateToday.addEventListener('input', () => {
+        console.log(dateStart.value);
+    });
+
+    if(nn(dateStartVal) || nn(numberOfYearsVal) || nn(dateTodayVal)){
+
+    }
+
+    // Calculate weeks
+    // let vHeader = document.getElementById("v-Header");
+
 });
+
+function nn(val) {
+    if(val != null){
+        return true;
+    }
+    return false;
+}
